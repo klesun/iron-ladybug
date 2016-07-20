2ec24da2cdee018b09d4e33c235aee66dae45151
Artur Klesun <arturklesun@gmail.com>
2016-07-21 02:30:45 +0300

    Add scene with some trials to get to the top of a mountain... another

--- Assets/Handmade/Scripts/HeroControl.cs
+++ Assets/Handmade/Scripts/HeroControl.cs
@@ -5,11 +5,19 @@ public class HeroControl : MonoBehaviour {
 
 	public float mouseSensitivity = 4.0F;
 
+	Rigidbody m_Rigidbody;
+
 	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0));
+
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			m_Rigidbody.velocity += transform.forward * 20;
+		}
 	}
 }

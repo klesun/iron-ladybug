2cfcba7aecc3c7d21af1e830a258ea25c5035c9e
Artur Klesun <arturklesun@gmail.com>
2016-07-28 03:01:29 +0300

    Add collectable strawberries on the vilage scene

---
+++ Assets/Handmade/Prefabs/Strawberry/Logic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour 
+{
+	public AudioSource collectedSound;
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("strawberry initialized!");
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		print ("strawberry collected!");
+		collectedSound.Play ();
+		Destroy (transform.parent.gameObject);
+	}
+}

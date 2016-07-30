91d1bc8a7b69ce263b2813a26bea3e421b1d6aa6
Artur Klesun <arturklesun@gmail.com>
2016-07-30 16:00:46 +0300

    Make hero say PARKOUR when he jumps

--- Assets/Handmade/Prefabs/Strawberry/Logic.cs
+++ Assets/Handmade/Prefabs/Strawberry/Logic.cs
@@ -5,15 +5,8 @@ public class Logic : MonoBehaviour
 {
 	public AudioSource collectedSound;
 
-	// Use this for initialization
-	void Start () 
-	{
-		print ("strawberry initialized!");
-	}
-
 	void OnCollisionEnter(Collision collision)
 	{
-		print ("strawberry collected!");
 		collectedSound.Play ();
 		Destroy (transform.parent.gameObject);
 	}
--- Assets/Handmade/Scripts/HeroControl.cs
+++ Assets/Handmade/Scripts/HeroControl.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroControl : MonoBehaviour {
-
+public class HeroControl : MonoBehaviour 
+{
 	public float mouseSensitivity = 4.0F;
+	public AudioSource jumpingSound;
 
 	Rigidbody m_Rigidbody;
 
-	void Start () {
+	void Start () 
+	{
 		m_Rigidbody = GetComponent<Rigidbody>();
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		transform.Rotate (new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0));
+		HandleKeys ();
+	}
 
+	private void HandleKeys()
+	{
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
 			m_Rigidbody.velocity += transform.forward * 20;
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			m_Rigidbody.velocity += Vector3.up * 10;
+			jumpingSound.Play ();
 		}
 	}
 }

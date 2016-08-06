0af12ccca80d76b2a84fc600b07b118a80cc6bde
Artur Klesun <arturklesun@gmail.com>
2016-08-07 02:01:42 +0300

    Replace default playable character model with the thing my sis made

--- Assets/Handmade/Scripts/HeroControl.cs
+++ Assets/Handmade/Scripts/HeroControl.cs
@@ -15,13 +15,17 @@ public class HeroControl : MonoBehaviour
 	public AudioClip outOfManaEvilSound;
 	public Transform gunShotSource;
 
+	public Animator anima;
+
 	private float? lastSprintTime = null;
 	private float distToGround;
+	private Rigidbody body;
 
 	void Start () 
 	{
 		Cursor.lockState = CursorLockMode.Locked;
 		distToGround = GetComponent<Collider> ().bounds.extents.y;
+		body = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -34,20 +38,24 @@ public class HeroControl : MonoBehaviour
 	private void HandleKeys()
 	{
 		if (IsGrounded()) {
+			anima.SetBool ("isFlying", false);
 			if (Input.GetKeyDown(KeyCode.Space)) {
-				GetComponent<Rigidbody>().velocity += Vector3.up * 10;
+				//anima.Play ("Armature|jump", -1, 0f);
+				anima.SetBool ("isFlying", true);
+				body.velocity += Vector3.up * 10;
 
 				var snd = Random.Range(0, 10) == 0
 					? jumpingEvilSound
 					: jumpingSound;
 
 				AudioSource.PlayClipAtPoint(snd, transform.position);
-				//AudioSource.PlayClipAtPoint(jumpingSfx, transform.position);
+				AudioSource.PlayClipAtPoint(jumpingSfx, transform.position);
 			}
 			if (Input.GetKeyDown (KeyCode.Mouse1)) {
 				AudioSource.PlayClipAtPoint(outOfManaEvilSound, transform.position);
 			}
 		} else {
+			anima.SetBool ("isFlying", true);
 			if (Input.GetKeyDown(KeyCode.Mouse0)) {
 				if (lastSprintTime == null || 
 					Time.fixedTime - lastSprintTime > SPRINT_INTERVAL
@@ -61,11 +69,31 @@ public class HeroControl : MonoBehaviour
 				}
 			}
 		}
+
+		var factor = 1f;
+		if (Input.GetKey(KeyCode.W)) {
+			body.velocity += transform.forward * factor;
+			//transform.Translate (Vector3.forward * 0.05f);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			body.velocity -= transform.forward;
+			//transform.Translate (Vector3.forward * -0.05f);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			body.velocity -= transform.right * factor;
+			//transform.Translate (Vector3.right * -0.05f);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			body.velocity += transform.right * factor;
+			//transform.Translate (Vector3.right * 0.05f);
+		}
+
+		anima.SetFloat ("xSpeed", body.velocity.x);
+		anima.SetFloat ("ySpeed", body.velocity.z);
 	}
 
 	bool IsGrounded()
 	{
 		return Physics.Raycast (transform.position, -Vector3.up, distToGround + 0.1f);
 	}
-
 }

80765a0521edb8a36c1a721590534e0301c18f5e
Ilya Gursky <dogzy123@mail.ru>
2016-08-06 21:23:01 +0300

    Add script for platforms and fix bugs

--- Assets/Handmade/Scripts/ObjectSettings.cs
+++ Assets/Handmade/Scripts/ObjectSettings.cs
@@ -3,32 +3,29 @@ using System.Collections;
 
 public class ObjectSettings : MonoBehaviour {
 
-
-	public int objectCount = 1;
-	public float spacing = 1;
-	public GameObject original;
-	public Mesh gizmoMesh = null;
+	public float spacing;
+	public GameObject originalMesh;
+	public Transform point;
 
 	void Start () {
-		PositionElements ((pos, rot) => Object.Instantiate (original, pos, rot));
+		PosForObjects ((pos, rot) => Object.Instantiate(originalMesh, pos, rot));
 	}
 
-	delegate void ElMaker(Vector3 pos, Quaternion rot);
-	void PositionElements(ElMaker makeEl)
-	{
-		for (int i = 0; i < objectCount; i++) {
-			var pos = original.transform.position + Vector3.forward * spacing * i;
-			var rot = original.transform.rotation;
-			makeEl(pos, rot);
+	delegate void CreateObj (Vector3 pos, Quaternion rot);
+
+	void PosForObjects (CreateObj makeObj) {
+		Vector3 startPos = transform.position;
+		Vector3 endPos = point.transform.position;
+		int objc = (int)(Vector3.Distance (endPos, startPos) / spacing);
+		for (float i = 0; i < objc; i++) {
+			var drawPos = Vector3.Lerp (startPos, endPos, i / objc);
+			makeObj (drawPos, new Quaternion (0,0,0,0));
 		}
+
 	}
 
 	void OnDrawGizmos () {
-		PositionElements ((pos, rot) => Gizmos.DrawWireCube(pos, new Vector3 (1,1,1)));
+		PosForObjects ((pos, rot) => Gizmos.DrawWireCube (pos, new Vector3 (1,1,1)));
 	}
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }

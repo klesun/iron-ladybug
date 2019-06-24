6c6c21093c6dbad912e5bdc6495fa50f56d33c7c
Artur Klesun <arurklesun@gmail.com>
2019-06-25 01:09:19 +0300

    Implement berryCurve.prefab

---
+++ Assets/ArrSlider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Util.Architecture;
+using UnityEngine;
+
+/**
+ * takes a GameObject and a _Train_ component and arranges
+ * copies of the n game object equally alongside the curve
+ */
+[ExecuteInEditMode]
+public class ArrSlider : MonoBehaviour
+{
+	public GameObject wagonHolder;
+	public GameObject wagonRef;
+	public int copyCnt = 1;
+	public Train train;
+
+#if UNITY_EDITOR
+	void OnValidate()
+	{
+		UnityEditor.EditorApplication.delayCall += () => {
+			if (!Application.isPlaying) {
+				Renew();
+			}
+		};
+	}
+#endif
+	
+	void Renew() {
+		var deadmen = new List<GameObject>();
+		foreach (Transform ch in wagonHolder.transform) {
+			deadmen.Add (ch.gameObject);
+		}
+		deadmen.ForEach (DestroyImmediate);
+
+		if (copyCnt > 0) {
+			train.spacing = 1f / copyCnt;
+		}
+		var copies = new List<Transform>();
+		for (var i = 0; i < copyCnt; ++i) {
+			var copy = Instantiate (wagonRef.gameObject, wagonHolder.transform);
+			copy.name = "_wagon" + i;
+			copies.Add(copy.transform);
+		}
+		train.wagons = copies.ToArray();
+		train.UpdatePosition(0);
+	}
+}
--- Assets/Scripts/Util/Architecture/Train.cs
+++ Assets/Scripts/Util/Architecture/Train.cs
@@ -1,4 +1,6 @@
+using System;
 using Assets.Scripts.Interfaces;
+using Assets.Scripts.Util.Logic;
 using UnityEngine;
 
 namespace Assets.Scripts.Util.Architecture {
@@ -14,9 +16,18 @@ namespace Assets.Scripts.Util.Architecture {
         public float spacing = 0.05f;
         [Range(0,1)]
         public float offset = 0;
-
+        public float speed = 1f / 180;
+        public TransformListener[] transformListeners = {};
 
 #if UNITY_EDITOR
+        private void OnEnable()
+        {
+            foreach (var listener in transformListeners) {
+                Console.WriteLine("ololo listener");
+                listener.onChange = () => UpdatePosition(0);
+            }
+        }
+
         void OnValidate()
         {
             UnityEditor.EditorApplication.delayCall += () => {
@@ -27,12 +38,17 @@ namespace Assets.Scripts.Util.Architecture {
         }
 #endif
 
-        void UpdatePosition(float progress)
+        public void UpdatePosition(float progress)
         {
             for (var i = 0; i < wagons.Length; ++i) {
+                var wagon = wagons[i];
+                if (!wagon) {
+                    // if this element was removed from scene,
+                    // if it was a collected berry for example
+                    continue;
+                }
                 var spacing = (wagons.Length - i - 1) * this.spacing;
                 var wagonProgress = (progress + offset + spacing) % 1;
-                var wagon = wagons[i];
                 trajectory.GetAt(wagonProgress).get
                 = point => wagon.position = point;
                 trajectory.GetAt(wagonProgress + 0.00001f).get
@@ -42,7 +58,7 @@ namespace Assets.Scripts.Util.Architecture {
 
         void FixedUpdate()
         {
-            UpdatePosition(Time.fixedTime / 180);
+            UpdatePosition(speed * Time.fixedTime);
         }
     }
 }
